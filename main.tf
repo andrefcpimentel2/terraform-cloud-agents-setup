a19bfe0f7c61e1a2c59660a72f770c3af14e08d2
andrefcpimentel2 <andre@hashicorp.com>
2021-10-13 16:29:01 +0100

    Initial commit

---
+++ main.tf
@@ -0,0 +1,44 @@
+
+resource "aws_iam_instance_profile" "agent_profile" {
+  name = "${var.namespace}-instance-profile"
+  role = aws_iam_role.agent-role.name
+}
+
+resource "aws_instance" "agent" {
+  count = var.instances
+  ami           = var.ami
+  instance_type = var.instance_type_worker
+  key_name      = aws_key_pair.deployer.key_name
+  user_data     = templatefile("templates/user-data.tpl", {
+      TFC_AGENT_TOKEN       = var.TFC_AGENT_TOKEN,
+      TFC_AGENT_NAME = "${var.TFC_AGENT_NAME}-${count.index}"
+  })
+  subnet_id              = aws_subnet.agent_subnet[0].id
+  vpc_security_group_ids = [aws_security_group.agent_sg.id]
+  iam_instance_profile = aws_iam_instance_profile.agent_profile.name
+
+  root_block_device{
+    volume_size           = "50"
+    delete_on_termination = "true"
+  }
+
+   ebs_block_device  {
+    device_name           = "/dev/xvdd"
+    volume_type           = "gp2"
+    volume_size           = "50"
+    delete_on_termination = "true"
+  }
+
+
+  tags = local.common_tags
+}
+
+resource "aws_eip" "agent" {
+  count = var.instances
+  instance =  element(aws_instance.agent.*.id, count.index)
+}
+
+
+output "EIP" {
+  value       = aws_eip.agent.*.public_ip
+}
